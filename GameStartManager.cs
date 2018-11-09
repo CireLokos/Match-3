bf3f1f0e04d641af742a6776c1b20d42ba2d0f8d
CireLokos <44911848+CireLokos@users.noreply.github.com>
2018-11-09 16:45:16 -0700

    Add files via upload

---
+++ GameStartManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartManager : MonoBehaviour
+{
+    public GameObject startPanel;
+    public GameObject levelPanel;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPanel.SetActive(true);
+        levelPanel.SetActive(false);
+	}
+	
+    public void PlayGame()
+    {
+        startPanel.SetActive(false);
+        levelPanel.SetActive(true);
+    }
+
+    public void Home()
+    {
+        startPanel.SetActive(true);
+        levelPanel.SetActive(false);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ SplashToTitle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SplashToTitle : MonoBehaviour {
+
+
+	void Start () {
+		StartCoroutine (SokolToMenu());
+	}
+
+	IEnumerator SokolToMenu()
+	{
+		yield return new WaitForSeconds (4);
+		SceneManager.LoadScene (1);
+	}
+}
+
